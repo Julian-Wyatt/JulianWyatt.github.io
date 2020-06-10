fcd7ab2811132fe960341f21969fbecdaf706832
Julian Wyatt <julian.wyatt@durham.ac.uk>
2020-06-10 21:55:13 +0100

    SE page and carousel working

---
+++ assets/js/carousel.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".carouselClass").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+});
