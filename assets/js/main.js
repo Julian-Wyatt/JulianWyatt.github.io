b3a86e54896d73f4b9e93c0308ad9464963ec173
Julian Wyatt <julian.wyatt@durham.ac.uk>
2020-06-14 21:14:09 +0100

    removed scroll animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,39 +10,39 @@
 
 	// http://thenewcode.com/279/Rotate-Elements-on-Scroll-with-JavaScript
 	// scrollAnimation event is used from the above link
-	;(function() {
-		var throttle = function(type, name, obj) {
-			var obj = obj || window;
-			var running = false;
-			var func = function() {
-				if (running) { return; }
-				running = true;
-				requestAnimationFrame(function() {
-					obj.dispatchEvent(new CustomEvent(name));
-					running = false;
-				});
-			};
-			obj.addEventListener(type, func);
-		};
-		throttle ("scroll", "scrollAnimation");
-	})();
-	let hero = document.getElementById("hero");
-  window.addEventListener("scrollAnimation", function(){
-	  if (window.screen.width > 1000){
-		if (window.pageYOffset <0){
-			// do nothing!!! ie do not negatively rotate
-		  }
-		  else if (window.pageYOffset < window.innerHeight*0.75){
-			//   rotate amount
-			let rotation = 120 * (window.pageYOffset/window.innerHeight);
-			hero.style.transform = "rotateX("+rotation+"deg)";
-			hero.style.webkitTransform = "rotateX("+rotation+"deg)";
-		  }
-		  else{
-			hero.style.transform = "rotateX(90deg)";
-			hero.style.webkitTransform = "rotateX(90deg)";
-		  }
-	  }
+// 	;(function() {
+// 		var throttle = function(type, name, obj) {
+// 			var obj = obj || window;
+// 			var running = false;
+// 			var func = function() {
+// 				if (running) { return; }
+// 				running = true;
+// 				requestAnimationFrame(function() {
+// 					obj.dispatchEvent(new CustomEvent(name));
+// 					running = false;
+// 				});
+// 			};
+// 			obj.addEventListener(type, func);
+// 		};
+// 		throttle ("scroll", "scrollAnimation");
+// 	})();
+// 	let hero = document.getElementById("hero");
+//   window.addEventListener("scrollAnimation", function(){
+// 	  if (window.screen.width > 1000){
+// 		if (window.pageYOffset <0){
+// 			// do nothing!!! ie do not negatively rotate
+// 		  }
+// 		  else if (window.pageYOffset < window.innerHeight*0.75){
+// 			//   rotate amount
+// 			let rotation = 120 * (window.pageYOffset/window.innerHeight);
+// 			hero.style.transform = "rotateX("+rotation+"deg)";
+// 			hero.style.webkitTransform = "rotateX("+rotation+"deg)";
+// 		  }
+// 		  else{
+// 			hero.style.transform = "rotateX(90deg)";
+// 			hero.style.webkitTransform = "rotateX(90deg)";
+// 		  }
+// 	  }
   });
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
