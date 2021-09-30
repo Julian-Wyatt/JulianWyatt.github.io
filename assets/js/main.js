42267a184a45b78041242a11aaa03a80998df00e
Julian Wyatt <julian.wyatt@durham.ac.uk>
2021-09-30 16:01:44 +0100

    updated fade in animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
+// https://stackoverflow.com/questions/27462306/css3-animate-elements-if-visible-in-viewport-page-scroll
+
+const inViewport = (entries, observer) => {
+	entries.forEach(entry => {
+	  entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+	});
+  };
+  
+  const Obs = new IntersectionObserver(inViewport);
+  const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+  
+  // Attach observer to every [data-inviewport] element:
+  const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+  ELs_inViewport.forEach(EL => {
+	Obs.observe(EL, obsOptions);
+  });
+
+
+
 /**
 * Template Name: Laura - v2.0.0
 * Template URL: https://bootstrapmade.com/laura-free-creative-bootstrap-theme/
