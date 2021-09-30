988f70afca0ac26f9437e6dbec8508fb4c2cce89
Julian Wyatt <julian.wyatt@durham.ac.uk>
2021-09-30 18:03:18 +0100

    edited anim

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,23 @@
 // https://stackoverflow.com/questions/27462306/css3-animate-elements-if-visible-in-viewport-page-scroll
 
-const inViewport = (entries, observer) => {
-	entries.forEach(entry => {
-	  entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+if(!!window.IntersectionObserver)
+{
+
+	const inViewport = (entries, observer) => {
+		entries.forEach(entry => {
+		entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+		});
+	};
+	
+	const Obs = new IntersectionObserver(inViewport);
+	const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+	
+	// Attach observer to every [data-inviewport] element:
+	const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+	ELs_inViewport.forEach(EL => {
+		Obs.observe(EL, obsOptions);
 	});
-  };
-  
-  const Obs = new IntersectionObserver(inViewport);
-  const obsOptions = {threshold: 0,rootMargin:'500px'}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
-  
-  // Attach observer to every [data-inviewport] element:
-  const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
-  ELs_inViewport.forEach(EL => {
-	Obs.observe(EL, obsOptions);
-  });
+}
 
 
 /**
